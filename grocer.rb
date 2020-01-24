3160d3e6059edcc8ada6b51c144f5eb627cab4cb
Lee McCormick <lzm0010@gmail.com>
2020-01-24 17:24:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,6 +36,8 @@ def apply_coupons(cart, coupons)
   while coupon_idx < coupons.size do
     cart_idx = 0
     while cart_idx < cart.size do
+      pp cart_idx
+      pp cart
       if cart[cart_idx][:item] == coupons[coupon_idx][:item]
         amount_of_item_in_cart = cart[cart_idx][:count]
         coupon_count = coupons[coupon_idx][:num]
