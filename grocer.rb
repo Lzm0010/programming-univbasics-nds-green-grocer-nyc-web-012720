75e8ec31a5ba1fb958f5c894256020fff0097f8f
Lee McCormick <lzm0010@gmail.com>
2020-01-24 17:16:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,6 +39,7 @@ def apply_coupons(cart, coupons)
       if cart[cart_idx][:item] == coupons[coupon_idx][:item]
         amount_of_item_in_cart = cart[cart_idx][:count]
         coupon_count = coupons[coupon_idx][:num]
+        pp coupons
         if amount_of_item_in_cart >= coupon_count
           cart[cart_idx][:count] = amount_of_item_in_cart - coupon_count
           coupons[coupon_idx][:count] = coupon_count
